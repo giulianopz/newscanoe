c1a2a022cebf52b35480e1c506ec4bfaf8012e70
giuliano <panzironi.giuliano@gmail.com>
2023-03-26 22:34:23 +0200

    Use stdlib funcs to get config and cache dirs

--- pkg/util/paths.go
+++ pkg/util/paths.go
@@ -3,21 +3,15 @@ package util
 import (
 	"os"
 	"path/filepath"
-
-	"github.com/giulianopz/newscanoe/pkg/app"
 )
 
 const (
-	xdgConfigDirEnvVar   = "XDG_CONFIG_HOME"
-	xdgCacheDirEnvVar    = "XDG_CACHE_HOME"
-	defaultConfigDirName = ".config"
-	defaultCacheDirName  = ".cache"
-	urlsFileName         = "urls"
-	cacheFileName        = "cache.gob"
+	urlsFileName  = "urls"
+	cacheFileName = "cache.gob"
 )
 
 func GetUrlsFilePath() (string, error) {
-	dirName, err := getConfigDir()
+	dirName, err := os.UserConfigDir()
 	if err != nil {
 		return "", err
 	}
@@ -25,30 +19,9 @@ func GetUrlsFilePath() (string, error) {
 }
 
 func GetCacheFilePath() (string, error) {
-	dirName, err := getConfigDir()
+	dirName, err := os.UserConfigDir()
 	if err != nil {
 		return "", err
 	}
 	return filepath.Join(dirName, cacheFileName), nil
-}
-
-func getConfigDir() (string, error) {
-	return getDir(xdgConfigDirEnvVar, defaultConfigDirName)
-}
-
-func getCacheDir() (string, error) {
-	return getDir(xdgCacheDirEnvVar, defaultCacheDirName)
-}
-
-func getDir(xdgEnvVar, defaultName string) (string, error) {
-
-	dir := os.Getenv(xdgEnvVar)
-	if dir == "" || dir[0:1] != "/" {
-		homeDir, err := os.UserHomeDir()
-		if err != nil {
-			return "", err
-		}
-		return filepath.Join(homeDir, defaultName, app.Name), nil
-	}
-	return filepath.Join(dir, app.Name), nil
 }
